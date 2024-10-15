be49aac472b787447587789207b37b1dabf030b8
Mateus Alves Lopes <mateusneny@hotmail.com>
2024-10-14 23:15:19 -0300

    Day 4b
    Adding new challenges: predecessor and successor, random number generator, and BRL to USD converter.

---
+++ ex010/antecessor-e-sucessor.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M01 Aula 27b – Cinco desafios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <p>Curso PHP Moderno com Gustavo Guanabara</p>
+        <p>Aluno: Mateus Alves Lopes</p>
+    </header>
+
+    <section>
+        <h1>Antecessor e Sucessor</h1>
+        <br>
+        <div class="flex">
+            <div class="formulario">
+                <h2>Informe um número</h2>
+                <p>Maior que 1</p>
+                <div class="form">
+                    <form action="desafio1.php" method="get">
+                        <label for="numero">Número</label>
+                        <input type="text" name="numero" id="idnumero">
+                        <input type="submit" value="Calcular">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <footer>
+        <p>Copyright&copy; <?= date('Y'); ?> - Todos os direitos reservados | Desenvolvido com ❤ e ☕︎ por <a
+                href="https://codech.com.br/">Mateus Lopes</a></p>
+        <div class="social">
+            <a href="https://www.linkedin.com/in/mateus-lopes-b76a31270/" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="LinkedIn"
+                    style="width: 40px; height: 40px;"></a>
+
+            <a href="https://www.behance.net/mateuslopes17" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=13655&format=png&color=000000" alt="Behance"
+                    style="width: 40px; height: 40px;"></a>
+
+            <a href="https://github.com/nenyrs" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=63777&format=png&color=000000" alt="GitHub"
+                    style="width: 40px; height: 40px;"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ ex010/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M01 Aula 27b – Cinco desafios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <p>Curso PHP Moderno com Gustavo Guanabara</p>
+        <p>Aluno: Mateus Alves Lopes</p>
+    </header>
+
+    <section>
+        <h1>Cinco desafios</h1>
+        <br>
+        <div>
+            <p>Desafio - <a href="http://localhost/Curso-Php-Moderno/ex010/antecessor-e-sucessor.php">Antecessor e
+                    Sucessor</a>
+            </p>
+            <p>Desafio - <a href="http://localhost/Curso-Php-Moderno/ex010/sorteador-de-numeros.php">Sorteador de
+                    Numeros</a>
+            </p>
+            <p>Desafio - <a href="http://localhost/Curso-Php-Moderno/ex010/conversor-de-moedas-v1.php">Conversor de
+                    Moedas v1</a>
+            </p>
+            <p>Desafio - <a href="http://localhost/Curso-Php-Moderno/ex010/conversor-de-moedas-v2.php">Conversor de
+                    Moedas v2</a>
+            </p>
+        </div>
+
+    </section>
+
+    <footer>
+        <p>Copyright&copy; <?= date('Y'); ?> - Todos os direitos reservados | Desenvolvido com ❤ e ☕︎ por <a
+                href="https://codech.com.br/">Mateus Lopes</a></p>
+        <div class="social">
+            <a href="https://www.linkedin.com/in/mateus-lopes-b76a31270/" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="LinkedIn"
+                    style="width: 40px; height: 40px;"></a>
+
+            <a href="https://www.behance.net/mateuslopes17" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=13655&format=png&color=000000" alt="Behance"
+                    style="width: 40px; height: 40px;"></a>
+
+            <a href="https://github.com/nenyrs" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=63777&format=png&color=000000" alt="GitHub"
+                    style="width: 40px; height: 40px;"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
